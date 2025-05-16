5f24e7460b338173709017ca85cada529aec018c
Vicky Parmar <vickyparmar@Vickys-MBA-M2.fritz.box>
2025-05-16 17:46:17 +0200

    fixing test

--- tests/tests.py
+++ tests/tests.py
@@ -11,6 +11,6 @@ from streamlit.testing.v1 import AppTest
 def test_login():
     at = AppTest.from_file('tests/app.py').run()
     at.text_input[0].input('jsmith').run()
-    at.text_input[1].input('abc').run()
+    at.text_input[1].input('abcDEF123$').run()
     at.button[0].click().run()
     assert 'jsmith' in at.session_state['username']
