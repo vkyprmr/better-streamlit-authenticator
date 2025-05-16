d9b961dc3789eb1cd1d0d4ad0d5ce78a2277b5e8
Vicky Parmar <vickyparmar@Vickys-MBA-M2.fritz.box>
2025-05-16 17:52:33 +0200

    fixing test 3

--- tests/tests.py
+++ tests/tests.py
@@ -10,7 +10,7 @@ from streamlit.testing.v1 import AppTest
 
 def test_login():
     at = AppTest.from_file('tests/app.py').run()
-    at.text_input[0].input('jsmith').run()
-    at.text_input[1].input('abcDEF123$').run()
+    at.text_input[0].input('test').run()
+    at.text_input[1].input('ABCdef123$$').run()
     at.button[0].click().run()
     assert 'jsmith' in at.session_state['username']
