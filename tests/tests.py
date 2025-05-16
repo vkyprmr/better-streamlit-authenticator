6c2cdd190ff3bf786eb3675d8f98072f2bc2c846
Vicky Parmar <vickyparmar@Vickys-MBA-M2.fritz.box>
2025-05-16 17:57:14 +0200

    fixing test 4

--- tests/tests.py
+++ tests/tests.py
@@ -9,8 +9,8 @@ Libraries imported:
 from streamlit.testing.v1 import AppTest
 
 def test_login():
-    at = AppTest.from_file('tests/app.py').run()
-    # at.text_input[0].input('test').run()
-    # at.text_input[1].input('ABCdef123$$').run()
-    # at.button[0].click().run()
-    # assert 'jsmith' in at.session_state['username']
+    at = AppTest.from_file('app.py').run()
+    at.text_input[0].input('test').run()
+    at.text_input[1].input('ABCdef123$$').run()
+    at.button[0].click().run()
+    assert 'jsmith' in at.session_state['username']
